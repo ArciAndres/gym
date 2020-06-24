001467334a4af01b6de938d479f704a418dd63a7
ArciAndres <arciandres@gmail.com>
2020-06-24 12:50:40 +0200

    Original reach.py

--- gym/envs/robotics/fetch/reach2.py
+++ gym/envs/robotics/fetch/reach2.py
@@ -1,22 +1,22 @@
 import os
 from gym import utils
-from gym.envs.robotics import fetch_env2
+from gym.envs.robotics import fetch_env
 
 
 # Ensure we get the path separator correct on windows
-MODEL_XML_PATH = os.path.join('fetch', 'reach2.xml')
+MODEL_XML_PATH = os.path.join('fetch', 'reach.xml')
 
 
-class FetchReach2ArmsEnv(fetch_env2.FetchEnv2, utils.EzPickle):
+class FetchReachEnv(fetch_env.FetchEnv, utils.EzPickle):
     def __init__(self, reward_type='sparse'):
         initial_qpos = {
-            'robot0:slide0': 0.0,
-            'robot0:slide1': 0.0,
+            'robot0:slide0': 0.4049,
+            'robot0:slide1': 0.48,
             'robot0:slide2': 0.0,
         }
-        fetch_env2.FetchEnv2.__init__(
+        fetch_env.FetchEnv.__init__(
             self, MODEL_XML_PATH, has_object=False, block_gripper=True, n_substeps=20,
-            gripper_extra_height=0.0, target_in_the_air=True, target_offset=0.0,
+            gripper_extra_height=0.2, target_in_the_air=True, target_offset=0.0,
             obj_range=0.15, target_range=0.15, distance_threshold=0.05,
             initial_qpos=initial_qpos, reward_type=reward_type)
         utils.EzPickle.__init__(self)
